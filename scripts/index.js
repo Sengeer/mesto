33c62ffea4ad6d4da577358974974d7fad4ea03e
@Sengeer | Sergey Polenov <fox8911@gmail.com>
2023-04-14 14:08:07 +0300

    feat: реализовать template в вёрстке и в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,46 @@
-let openPopupBtn = document.querySelector('.profile__edit-btn');
-let popup = document.querySelector('.popup');
-let popupCloseBtn = popup.querySelector('.popup__close-btn');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
+const openPopupBtn = document.querySelector('.profile__edit-btn');
+const popup = document.querySelector('.popup');
+const popupCloseBtn = popup.querySelector('.popup__close-btn');
 
-let formElement = popup.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_modal-type_name');
-let descriptionInput = formElement.querySelector('.popup__input_modal-type_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_modal-type_name');
+const descriptionInput = formElement.querySelector('.popup__input_modal-type_description');
 
 function openPopup() {
   popup.classList.add('popup_opened');
 
-  let textName = profileName.textContent;
-  let textDescription = profileDescription.textContent;
+  const textName = profileName.textContent;
+  const textDescription = profileDescription.textContent;
 
   nameInput.value = textName;
   descriptionInput.value = textDescription;
@@ -29,8 +56,8 @@ popupCloseBtn.addEventListener('click', closePopup);
 function handleFormSubmit (evt) {
   evt.preventDefault();
 
-  let nameText = nameInput.value;
-  let descriptionText = descriptionInput.value;
+  const nameText = nameInput.value;
+  const descriptionText = descriptionInput.value;
 
   profileName.textContent = nameText;
   profileDescription.textContent = descriptionText;
@@ -38,3 +65,17 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+const imageTemplate = document.getElementById('image-template');
+const imageContainer = document.querySelector('.photo-place__elements');
+
+const addImageElement = function (imageData) {
+  const imageElement = imageTemplate.content.querySelector('.photo-place__element').cloneNode(true);
+
+  const elementImage = imageElement.querySelector('.photo-place__image');
+  const elementTitle = imageElement.querySelector('.photo-place__title');
+
+  elementImage.src = imageData.link;
+  elementImage.alt = imageData.name;
+  elementTitle.textContent = imageData.name;
+};
