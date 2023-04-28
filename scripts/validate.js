c3b23838846d206f1acaf2081f013f0847f7d0dc
@Sengeer | Sergey Polenov <fox8911@gmail.com>
2023-04-28 16:57:39 +0300

    feat: добавить в поля формы мин / макс длинну текста, отключить браузерную валидацию

---
+++ scripts/validate.js
@@ -0,0 +1,9 @@
+
+// enableValidation({
+//   formSelector: 'popup__form',
+//   inputSelector: 'popup__input',
+//   submitButtonSelector: 'popup__submit-btn',
+//   inactiveButtonClass: 'popup__submit-btn_inactive',
+//   inputErrorClass: 'popup__input-error',
+//   activeErrorClass: 'popup__input-error_active'
+// });
